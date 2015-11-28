3dbfb30f6b54c4e8544610d587a8bc6836ceffad
keklikhasan <keklikhasan@gmail.com>
2015-11-28 02:56:07 +0200

    feedback acitivity screen

--- app/src/main/java/com/gdgistanbul/devfest/feedback/activities/FeedbackActivity.java
+++ app/src/main/java/com/gdgistanbul/devfest/feedback/activities/FeedbackActivity.java
@@ -4,18 +4,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.gdgistanbul.devfest.feedback.R;
 import com.gdgistanbul.devfest.feedback.models.Session;
 import com.gdgistanbul.devfest.feedback.util.PrefUtils;
+import com.jakewharton.rxbinding.view.RxView;
 
+import java.util.concurrent.TimeUnit;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 import timber.log.Timber;
 
 public class FeedbackActivity extends AppCompatActivity {
 
   public static final String TAG = FeedbackActivity.class.getSimpleName();
+
+  @Bind(R.id.feedback_good)
+  protected View feedbackGoodView;
+  @Bind(R.id.feedback_neutral)
+  protected View feedbackNeutralView;
+  @Bind(R.id.feedback_bad)
+  protected View feedbackBadView;
 
   private Session session;
 
@@ -46,7 +60,22 @@ public class FeedbackActivity extends AppCompatActivity {
   }
 
   private void setupViews() {
+    RxView
+        .clicks(feedbackBadView)
+        .debounce(1000, TimeUnit.MILLISECONDS)
+        .subscribe(new Subscriber<Void>() {
+          @Override
+          public void onCompleted() {
+          }
 
+          @Override
+          public void onError(Throwable e) {
+          }
+
+          @Override
+          public void onNext(Void aVoid) {
+          }
+        });
   }
 
   @Override
